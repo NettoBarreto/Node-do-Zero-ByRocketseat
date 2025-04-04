18d45fcb7986770f526f6d6af4097ba507e7dbbe
Netto Barreto <nettobarretooficial@gmail.com>
2025-04-04 12:45:37 -0300

    Atualiza rotas de vídeo e define a porta do servidor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify'
 // import { DatabaseMemory } from './database-memory.js'
 import { DatabasePostgres } from './database-postgres.js'
+const port = process.env.PORT || 4000;
 
 const server = fastify()
 
